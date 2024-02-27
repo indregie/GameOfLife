d10a9d1483c96291d13b5fad7c30660075ca7af9
Indre Giedraityte <indre.giedraityte@accenture.com>
2024-02-27 10:21:21 +0200

    Added collecting of user input for board size

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Welcome to the game of life!\nPlease write down the maximum height of the desk input:");
+try
+{
+    string? inputDeskHeight = Console.ReadLine();
+
+    Console.WriteLine("Please write down the maximum lenght of the desk input:");
+    string? inputDeskLenght = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(inputDeskHeight) || string.IsNullOrEmpty(inputDeskLenght))
+    {
+        throw new Exception("Please provide valid input.");
+    }
+
+    int deskHeight = int.Parse(inputDeskHeight!);
+    int deskLenght = int.Parse(inputDeskLenght!);
+
+    int[,] board = new int[deskHeight, deskLenght];
+    Console.WriteLine($"board: {deskHeight}, {deskLenght}");
+}
+catch (Exception ex) 
+{
+    Console.WriteLine(ex.Message);
+}
